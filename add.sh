8a55b21e9f707c1e8463f2c521db6f35a68decb1
Jess Smith <smith.jessk@gmail.com>
2018-09-05 22:59:33 -0400

    Forget go, use shell!

---
+++ add.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+echo $1
+sudo mysql mason -e "create table if not exists memories
+    (memory MEDIUMBLOB NOT NULL,
+    added TIMESTAMP NOT NULL,
+    last_accessed TIMESTAMP)"
+sudo mysql mason -e "insert into memories (memory, added) VALUES (\"$1\", NOW())"
